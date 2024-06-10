c9e00957cb115ae5dec7b8e53689560f3c8e3505
Juan Arnaboldi <nocto2014@gmail.com>
2024-06-10 11:11:43 -0300

    this is it

--- auto_reset.py
+++ auto_reset.py
@@ -4,6 +4,8 @@ import ctypes
 import pydirectinput
 import time
 import pyperclip
+import pygetwindow as gw
+
 
 def run_as_admin():
     if sys.platform.startswith('win'):
@@ -27,36 +29,57 @@ if ctypes.windll.shell32.IsUserAnAdmin() == 0:
 pyperclip.copy('/')
 
 # Sleep for a bit
-for i in range(120, 0, -1):
+for i in range(10, 0, -1):
     print(i)
     time.sleep(1)
 
 while True:
-    # Press enter
-    pydirectinput.press('enter')
-    print("Pressed enter")
+    # Check window name
+    def get_window_title_by_keywords(keywords):
+        windows = gw.getAllWindows()
+        matching_windows = [window for window in windows if all(keyword.lower() in window.title.lower() for keyword in keywords)]
+        return [window.title for window in matching_windows]
 
-    # Delay before typing the word
-    time.sleep(2)
-    print("Waiting before typing...")
+    window_title = get_window_title_by_keywords(['Character', 'Level'])
+    print(window_title)
+    
+    # check if window_title contain certain keyword then do something
+    def reset_condition_in_window_title(window_title):
+        levels = ['Level: {}'.format(i) for i in range(380, 401)]
+        if any(level in window_title for level in levels):
+            # Press enter
+            pydirectinput.press('enter')
+            print("Pressed enter")
 
-    # Type the word
-    # Press down the Ctrl key
-    pydirectinput.keyDown('ctrl')
-    # Press the 'v' key
-    pydirectinput.press('v')
-    # Release the Ctrl key
-    pydirectinput.keyUp('ctrl')
-    word = "reset"
-    for char in word:
-        pydirectinput.press(char)
-    print(f"Typed: {word}")
+            # Delay before typing the word
+            time.sleep(2)
+            print("Waiting before typing...")
 
-    # Press enter again
-    pydirectinput.press('enter')
-    print("Pressed enter again")
+            # Type the word
+            # Press down the Ctrl key
+            pydirectinput.keyDown('ctrl')
+            # Press the 'v' key
+            pydirectinput.press('v')
+            # Release the Ctrl key
+            pydirectinput.keyUp('ctrl')
+            word = "reset"
+            for char in word:
+                pydirectinput.press(char)
+            print(f"Typed: {word}")
 
-    # Sleep for a bit
-    for i in range(120, 0, -1):
-        print(i)
-        time.sleep(1)
+            # Press enter again
+            pydirectinput.press('enter')
+            print("Pressed enter again")
+
+            # Sleep for a bit
+            for i in range(60, 0, -1):
+                print(i)
+                time.sleep(1)
+        else:
+            print("No reset condition found")
+            # Sleep for a bit
+            for i in range(60, 0, -1):
+                print(i)
+                time.sleep(1)
+            
+    reset_condition_in_window_title(window_title)
